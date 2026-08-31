d53c2c908a8ef886ef4460f069fcc3d6d6c85a31
Ivan Majstorović <ivanmajstorovic012@gmail.com>
2026-08-31 18:10:39 +0200

    Modify RS flip-flop testbench to include areset
    Added areset signal to the RS flip-flop testbench and updated initial conditions.

--- Verilog basics/RS flip flop/rs ff tb.v
+++ Verilog basics/RS flip flop/rs ff tb.v
@@ -1,48 +1,53 @@
+`timescale 1ns/1ps
+
 module rs_flip_flop_tb;
 
-    reg r;
-    reg s;
-    reg clk;
-    wire q;
+reg r;
+reg s;
+reg clk;
+reg areset;
+wire q;
 
-    rs_flip_flop dut (
-        .r(r),
-        .s(s),
-        .clk(clk),
-        .q(q)
-    );
+rs_flip_flop dut (
+    .r(r),
+    .s(s),
+    .clk(clk),
+    .areset(areset),
+    .q(q)
+);
 
-    always #5 clk = ~clk;
+always #5 clk = ~clk;
 
-    initial begin
-      $dumpfile("dump.vcd");
-      $dumpvars(0,rs_flip_flop_tb);
-        clk = 0;
-        r = 0;
-        s = 0;
+initial begin
+    $dumpfile("dump.vcd");
+    $dumpvars(0, rs_flip_flop_tb);
+end
 
-        #10;
-        r = 0;
-        s = 1;
+initial begin
+    clk = 0;
+    areset = 1;
+    r = 0;
+    s = 0;
 
-        #10;
-        r = 1;
-        s = 1;
+    #10;
+    areset = 0;
+    r = 0;
+    s = 1;
 
-        #10;
-        r = 1;
-        s = 0;
+    #10;
+    r = 1;
+    s = 1;
 
-        #10;
-        r = 0;
-        s = 0;
+    #10;
+    r = 1;
+    s = 0;
 
-        #10;
-        r = 0;
-        s = 0;
+    #10;
+    r = 0;
+    s = 0;
 
-        #10;
-        $finish;
-    end
+    #20;
+    $finish;
+end
 
 endmodule
