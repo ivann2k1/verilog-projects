9d8eb00b40d5dc7ac4933bfbb977c5c839aa238c
Ivan Majstorović <ivanmajstorovic012@gmail.com>
2026-08-31 18:10:20 +0200

    Add asynchronous reset to RS flip flop module

--- Verilog basics/RS flip flop/rs ff dut.v
+++ Verilog basics/RS flip flop/rs ff dut.v
@@ -1,22 +1,20 @@
-module rs_flip_flop(
-  
-  input r,
-  input s,
-  input clk,
-  output reg q
-  
-  
+module rs_flip_flop (
+    input r,
+    input s,
+    input clk,
+    input areset,
+    output reg q
 );
-  always@(posedge clk)begin
-    
-    if({r,s}==2'b11) begin
-      $display("Forbidden state");
-      q<=1'bx;
+
+always @(posedge clk or posedge areset) begin
+    if ({r, s} == 2'b11) begin
+        $display("FORBIDDEN STATE");
+        q <= 1'bx;
+    end else if (areset) begin
+        q <= 1'b0;
+    end else begin
+        q <= s | (~r & q);
     end
-    else begin
-      q<=s|(~r&q); 
-      
-    end
-    
-  end
+end
+
 endmodule
